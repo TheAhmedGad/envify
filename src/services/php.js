caed39e33b5633323f5d074a719cc32fb2f988a2
Islam Zekry <theizekry@gmail.com>
2024-04-25 02:51:10 +0200

    Refactored php.js job

--- src/services/php.js
+++ src/services/php.js
@@ -1,11 +1,10 @@
-import inquirer from 'inquirer'
-import chalk from 'chalk'
-import runner from '../utils/runner.js'
-import { Spinner } from '@topcli/spinner'
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import runner from '../utils/runner.js';
+import { Spinner } from '@topcli/spinner';
 
 const php = {
-  selected_version: '8.3',
-
+  selectedVersion: '8.3',
   extensions: {
     fpm: false,
     mbstring: false,
@@ -19,99 +18,57 @@ const php = {
   },
 
   async prepare() {
-    await inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'php_version',
-          message: 'Select PHP version',
-          choices: ['8.3', '8.2', '8.1', '7.4', '7.3', '7.2', '7.1', '7.0'],
-          filter(val) {
-            return val.toLowerCase()
-          }
+
+    const { phpVersion } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'phpVersion',
+        message: 'Select PHP version',
+        choices: ['8.3', '8.2', '8.1', '7.4', '7.3', '7.2', '7.1', '7.0'],
+        filter(val) {
+          return val.toLowerCase();
         }
-      ])
-      .then(answer => {
-        this.selected_version = answer.php_version
-      })
-    return this
+      }
+    ]);
+    this.selectedVersion = phpVersion;
+    return this;
   },
 
   async handle() {
-    return new Promise((resolve, reject) => {
-      const spinner = new Spinner().start(' Installing PHP')
-      runner
-        .run('sudo add-apt-repository -y ppa:ondrej/php')
-        .then(() => {
-          runner
-            .run('sudo apt update -y')
-            .then(() => {
-              runner
-                .run('sudo apt upgrade -y')
-                .then(() => {
-                  runner
-                    .run(`sudo apt-get -y install php${this.selected_version}`)
-                    .then(async () => {
-                      spinner.succeed(
-                        ` PHP installed (${spinner.elapsedTime.toFixed(2)}ms)`
-                      )
-                      for (const extension of Object.keys(this.extensions)) {
-                        const spinner = new Spinner().start(
-                          `   Installing php-${extension} `,
-                          { withPrefix: ' - ' }
-                        )
-                        await runner
-                          .run(
-                            `sudo apt-get -y install php${this.selected_version}-${extension}`
-                          )
-                          .then(() => {
-                            this.extensions[extension] = true
-                            spinner.succeed(
-                              `   php-${extension} installed (${spinner.elapsedTime.toFixed(2)}ms)`
-                            )
-                          })
-                          .catch(err => {
-                            spinner.failed(`   php-${extension} was not found`)
-                          })
-                      }
-                      resolve()
-                    })
-                    .catch(err => {
-                      spinner.failed('failed to install PHP')
-                      reject(err)
-                    })
-                })
-                .catch(err => {
-                  spinner.failed('failed to Upgrade ubuntu repos.')
-                  reject(err)
-                })
-            })
-            .catch(err => {
-              spinner.failed('failed to Update ubuntu repos.')
-              reject(err)
-            })
-        })
-        .catch(err => {
-          spinner.failed('failed to Add PHP repo.')
-          reject(err)
-        })
-    })
+    const spinner = new Spinner().start('Installing PHP');
+    try {
+      await runner.run('sudo add-apt-repository -y ppa:ondrej/php');
+      await runner.run('sudo apt update -y');
+      await runner.run('sudo apt upgrade -y');
+      await runner.run(`sudo apt-get -y install php${this.selectedVersion}`);
+      spinner.succeed(`PHP installed (${spinner.elapsedTime.toFixed(2)}ms)`);
+      for (const extension of Object.keys(this.extensions)) {
+        const extensionSpinner = new Spinner().start(`Installing php-${extension}`, { withPrefix: ' - ' });
+        try {
+          await runner.run(`sudo apt-get -y install php${this.selectedVersion}-${extension}`);
+          this.extensions[extension] = true;
+          extensionSpinner.succeed(`php-${extension} installed (${extensionSpinner.elapsedTime.toFixed(2)}ms)`);
+        } catch (err) {
+          extensionSpinner.failed(`php-${extension} was not found`);
+        }
+      }
+      return Promise.resolve();
+    } catch (err) {
+      spinner.failed('Failed to install PHP');
+      return Promise.reject(err);
+    }
   },
+
   async afterInstall() {
-    console.log(
-      chalk.dim('PHP Version: ') + chalk.green(`${this.selected_version}`)
-    )
+    console.log(chalk.dim('PHP Version: ') + chalk.green(`${this.selectedVersion}`));
 
-    console.log(
-      chalk.dim('installed PHP extensions : ') +
-        chalk.green(
-          `[${Object.entries(this.extensions)
-            .filter(o => o[1] === true)
-            .map(o => o[0])
-            .join(', ')}]`
-        )
-    )
+    const installedExtensions = Object.entries(this.extensions)
+      .filter(([_, installed]) => installed)
+      .map(([extension]) => extension)
+      .join(', ');
+
+    console.log(chalk.dim('Installed PHP extensions: ') + chalk.green(`[${installedExtensions}]`));
   }
-}
+};
 
-export { php }
+export { php };

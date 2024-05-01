e16132b230a50b615dc5a7453a7e720031171c1c
Ahmed Gad <eng.ahmedmgad@gmail.com>
2024-05-01 21:16:36 +0300

    add node, pm2 to laravel stack

--- src/stacks/laravel-stack.js
+++ src/stacks/laravel-stack.js
@@ -4,10 +4,12 @@ import { mysql } from '../services/mysql.js'
 import { php } from '../services/php.js'
 import { redis } from '../services/redis.js'
 import { composer } from '../services/composer.js'
+import { node } from '../services/node.js'
+import { pm2 } from '../services/pm2.js'
 
 const laravelStack = {
   name: 'laravel-stack',
-  services: [git, nginx, mysql, php, composer, redis],
+  services: [git, nginx, mysql, php, composer, redis, node, pm2],
   async collectStackServices() {}
 }
 
